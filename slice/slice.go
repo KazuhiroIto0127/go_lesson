7a5e5a101420fae84a6b0f4e99493e5641b84dc5
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-03-20 12:37:33 +0900

    slice 追加

---
+++ slice/slice.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := make([]string, 3)
+	fmt.Println("emp", s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println(s)
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println(s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy", c)
+	fmt.Println(c[2:5])
+}
