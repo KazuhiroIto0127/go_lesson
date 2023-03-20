e9e8f196f9d9a5138249ab0fdbac9c80e2dea860
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-03-20 12:03:28 +0900

    for, values 追加

---
+++ values/values.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+	fmt.Println("1+1=", 1+1)
+
+	var a = "initial"
+	fmt.Println(a)
+
+	appleString := "apple"
+	fmt.Println(appleString)
+}
