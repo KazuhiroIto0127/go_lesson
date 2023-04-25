3004c48f1fd7792c06f69d5bf8e380c897b6407c
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-04-26 08:48:46 +0900

    add iota.go

---
+++ playground/iota/iota.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	Apple = iota
+	Orange
+	Banana
+)
+
+func main() {
+	fmt.Println(Apple)  // 0
+	fmt.Println(Orange) // 1
+	fmt.Println(Banana) // 2
+}
