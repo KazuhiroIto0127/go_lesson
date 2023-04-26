2b99cd49dc3b38da609347b3aeb27bf4e7dfbd8e
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-04-26 20:59:12 +0900

    mapにfor range出力追加

--- playground/map/map.go
+++ playground/map/map.go
@@ -2,13 +2,22 @@ package main
 
 import "fmt"
 
+func printAllMapValue(m map[string]int) {
+	for k, v := range m {
+		fmt.Printf("key: %v, value: %v\n", k, v)
+	}
+}
+
 func main() {
 	m := make(map[string]int)
 	m["apple"] = 1
 	m["banana"] = 2
+	m["orange"] = 3
 	fmt.Println(m)
 	fmt.Println(m["apple"])
 	fmt.Println(len(m))
 	delete(m, "apple")
 	fmt.Println(m)
+
+	printAllMapValue(m)
 }
