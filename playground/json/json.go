9b6692aa6479bfbcab187ea1ccdc0ccace287f53
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-05-05 12:12:17 +0900

    encoding/jsonを使ったサンプル追加

---
+++ playground/json/json.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+var jsonString = `
+{
+	"species": "ハト",
+	"description": "岩にとまるのが好き",
+	"dimensions": {
+		"height": 24,
+		"width": 10
+	}
+}
+`
+
+type Dimensions struct {
+	Width int `json:"width"`
+	Height int `json:"height"`
+}
+
+type Data struct {
+	Species string `json:"species"`
+	Description string `json:"description"`
+	Dimensions Dimensions `json:"dimenstions"`
+}
+
+func main(){
+	data := Data{
+		Species: "つばめ",
+		Description: "小さい鳥",
+		Dimensions: Dimensions{
+			Height: 10,
+			Width: 20,
+		},
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal("Error encoding JSON:", err)
+	}
+	fmt.Println("JSON data:", string(jsonData))
+
+	var data2 Data
+	err = json.Unmarshal([]byte(jsonString), &data2)
+	if err != nil {
+		log.Fatal("Error decoding JSON:", err)
+	}
+
+	fmt.Println("Decode JSON data:", data2)
+}
