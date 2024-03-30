7ba57b07303577af845cb93b8f91f7c054dbdc65
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2024-03-30 09:26:07 +0000

    normalで計算結果を出すように修正

--- concurrency/normal.go
+++ concurrency/normal.go
@@ -5,20 +5,21 @@ import (
 	"time"
 )
 
-func fib(n int) int{
+func fib(n int) int {
 	if n <= 1 {
 		return n
 	}
-	return fib(n - 1) + fib(n - 2)
+	return fib(n-1) + fib(n-2)
 }
 
 func main() {
 	start := time.Now() // 実行前の時刻を取得
 
 	for i := 0; i < 5; i++ {
-		fib(40)
+		result := fib(40)
+		fmt.Printf("fib(40) = %d\n", result)
 	}
 
 	duration := time.Since(start) // 実行時間を計測
-	fmt.Printf("Execution time: %v\n", duration)
+	fmt.Printf("Total execution time: %v\n", duration)
 }
